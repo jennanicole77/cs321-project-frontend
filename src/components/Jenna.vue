1753b88bd24088ded191c67ac9130b2c3146ba04
jennashuler <jenna.shuler@specinnovations.com>
2021-10-14 15:01:17 -0400

    Vue Framework

---
+++ src/components/Jenna.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button @click="test">Click Me </button>
+        Data: {{meow}}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+        meow: null
+    }
+  },
+  methods: {
+      abort: function() {
+
+      },
+      test: function() {
+          this.meow = new Date();
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
